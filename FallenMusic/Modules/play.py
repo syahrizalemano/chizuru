4858e40f9feca6d38796489fcc4c63b09ae48558
Dev Arora <116826965+DevArora2604@users.noreply.github.com>
2023-02-17 18:12:00 +0530

    Update play.py

--- FallenMusic/Modules/play.py
+++ FallenMusic/Modules/play.py
@@ -179,7 +179,7 @@ async def play(_, message: Message):
         file_path = audio_dl(url)
     else:
         if len(message.command) < 2:
-            return await fallen.edit_text("» ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴɴᴀ ᴘʟᴀʏ ʙᴀʙʏ ?")
+            return await fallen.edit_text(f"» {gay}, ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴɴᴀ ᴘʟᴀʏ ʙᴀʙʏ ?")
         await fallen.edit_text(f"{gay} ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ʙᴀʙʏ! 🔍")
         query = message.text.split(None, 1)[1]
         try:
