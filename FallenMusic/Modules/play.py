a49774ff1d18d50560740f3f9c0ba44c212982b2
Dev Arora <115332419+Devarora-0981@users.noreply.github.com>
2023-02-17 06:34:17 +0530

    Update play.py

--- FallenMusic/Modules/play.py
+++ FallenMusic/Modules/play.py
@@ -66,7 +66,9 @@ from FallenMusic.Helpers.thumbnails import gen_qthumb, gen_thumb
     & ~filters.via_bot
 )
 async def play(_, message: Message):
-    fallen = await message.reply_text("» ᴘʀᴏᴄᴇssɪɴɢ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...")
+    gay = message.from_user.first_name
+    trans = message.from_user.user_id
+    fallen = await message.reply_text(f"» [{gay}](tg://user?user_id={trans}) ᴘʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...")
     try:
         await message.delete()
     except:
