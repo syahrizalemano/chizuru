232c1a784bed4f3baaaf8c06db8f61039bf4f4c9
Dev Arora <115332419+Devarora-0981@users.noreply.github.com>
2023-03-26 19:11:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,15 +8,15 @@ load_dotenv()
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None) #Loda Lele
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/461571fc6dcba3522e24b.mp4")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/bb969e13f0bf0bc3b8dbf.mp4")
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("SESSION", None) #Lund Ka Scammer Hai Tu Vai.
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
