c1455c8472b5d9d4e81201a0694a6edd45e82a25
Anobots <89107294+syahrizalemano@users.noreply.github.com>
2023-03-30 14:33:42 -0700

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/bb969e13f0bf0bc3b8dbf.mp4")
+PING_IMG = getenv("PING_IMG", "https://ibb.co/3cnmmJj")
+START_IMG = getenv("START_IMG", "https://ibb.co/3cnmmJj")
 
 SESSION = getenv("SESSION", None) #Lund Ka Scammer Hai Tu Vai.
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/wigglehobbit")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/F")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "13564690875").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://ibb.co/3cnmmJj"
